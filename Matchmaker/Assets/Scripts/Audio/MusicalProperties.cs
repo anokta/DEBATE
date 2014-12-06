ee27e6909011a0077f26de81ce26cfbbda8aa6ab
Alper Gungormusler <alpynaq@gmail.com>
2014-12-06 11:11:33 +0000

    First shout!

---
+++ Matchmaker/Assets/Scripts/Audio/MusicalProperties.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class MusicalProperties
+{
+    static float[] majorScale = { 0, 2, 4, 5, 7, 9, 11 };
+    static float[] harmonicMinorScale = { 0, 2, 3, 5, 7, 8, 11 };
+    static float[] naturalMinorScale = { 0, 2, 3, 5, 7, 8, 10 };
+    static Dictionary<MusicalScale, float[]> Scales = new Dictionary<MusicalScale, float[]>()
+        {
+            { MusicalScale.MAJOR, majorScale },
+            { MusicalScale.HARMONIC_MINOR, harmonicMinorScale },
+            { MusicalScale.NATURAL_MINOR, naturalMinorScale }
+        };
+
+    public static float[] CurrentScale = Scales[MusicalScale.MAJOR];
+
+    public static float GetRandomPitch()
+    {
+        int index = Random.Range(0, CurrentScale.Length);
+
+        return Mathf.Pow(1.05f, CurrentScale[index]);
+    }
+}
+
+public enum MusicalScale
+{
+    MAJOR = 0, HARMONIC_MINOR = 1, NATURAL_MINOR = 2
+}
---
+++ Matchmaker/Assets/Scripts/LonerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LonerController : MonoBehaviour {
+
+    const int MIN_SHOUT = 3;
+    const int MAX_SHOUT = 7;
+
+    AudioSource sample;
+    int shoutLength, shoutCurrent;
+
+	void Awake () 
+    {
+        sample = GetComponent<AudioSource>();
+        shoutLength = shoutCurrent = 0;
+    }
+	
+	void Update () {
+	    if(Input.GetButtonDown("Jump"))
+        {
+            Interact();
+        }
+
+        if(shoutCurrent < shoutLength && !sample.isPlaying)
+        {
+            sample.pitch = MusicalProperties.GetRandomPitch();
+            sample.Play();
+
+            shoutCurrent++;
+        }
+	}
+
+    void Interact()
+    {
+        shoutCurrent = 0;
+        shoutLength = Random.Range(MIN_SHOUT, MAX_SHOUT);
+    }
+}
